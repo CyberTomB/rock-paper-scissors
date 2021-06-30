be04cfc0154769310da140d87eb905cee1b487e8
CyberDack <afntwrk.guyrpg@gmail.com>
2021-06-30 15:33:33 -0600

    emoji display added

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
 const comparator = {
    rock: {
-      wins: ['lizard', 'scissors']
+      wins: ['lizard', 'scissors'],
+      emoji: '✊'
    },
    paper: {
-      wins: ['rock', 'spock']
+      wins: ['rock', 'spock'],
+      emoji: '🧻'
    },
    scissors: {
-      wins: ['paper', 'lizard']
+      wins: ['paper', 'lizard'],
+      emoji: '✂'
    },
    lizard: {
-      wins: ['paper', 'spock']
+      wins: ['paper', 'spock'],
+      emoji: '🦎'
    },
    spock: {
-      wins: ['rock', 'scissors']
+      wins: ['rock', 'scissors'],
+      emoji: '🖖'
    }
 }
 
 var compChoice = 'rock'
+var compEmoji = comparator.rock.emoji
 
 function randomCompChoice() {
    let compArr = Object.keys(comparator)
    let randIndex = Math.floor(Math.random() * (compArr.length))
    compChoice = compArr[randIndex]
+   compEmoji = comparator[compChoice].emoji
    console.log(compChoice)
 }
 
 function play(choice) {
    let result = ''
    console.log('pre-logic choices', choice, compChoice)
+   drawChoices(choice)
    if (choice == compChoice) {
       result = 'draw'
    } else {
-      winning = comparator[choice].wins.find(win => win == compChoice)
+      let winning = comparator[choice].wins.find(win => win == compChoice)
       if (winning) {
          result = 'win'
       } else {
@@ -59,6 +67,10 @@ function drawButtons() {
    document.getElementById('buttons').innerHTML = template
 }
 
+function drawChoices(choice) {
+   document.getElementById('my-choice').innerText = comparator[choice].emoji
+   document.getElementById('comp-choice').innerText = compEmoji
+}
 
 drawButtons()
 randomCompChoice()
